3d3b8fd1b79648a910e63812403ef94fbd66ee63
ekaterina-nbl <ekaterina.vergizova@nobitlost.com>
2017-06-07 10:29:54 +0300

    initial implementation

---
+++ bin/cmds/device/list.js
@@ -0,0 +1,61 @@
+// MIT License
+//
+// Copyright 2017 Electric Imp
+//
+// SPDX-License-Identifier: MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+const Device = require('../../../lib/Device');
+const Options = require('../../../lib/util/Options');
+
+const COMMAND = 'list';
+const COMMAND_SECTION = 'device';
+const COMMAND_DESCRIPTION = 'Displays info about all or filtered Devices available for a user';
+const COMMAND_OPTIONS = '[--unassigned] [--assigned] [--online] [--offline] [--product-id <product_id>] [--product-name <product_name>] ' +
+                        '[--dg-id <device_group_id>] [--dg-name <device_group_name>] [--help]';
+
+exports.command = COMMAND;
+
+exports.describe = COMMAND_DESCRIPTION;
+
+exports.builder = function (yargs) {
+    return yargs
+        .usage(Options.getUsage(COMMAND_SECTION, COMMAND, COMMAND_DESCRIPTION, COMMAND_OPTIONS))
+        .options(Options.getOptions({
+            [Options.UNASSIGNED] : false,
+            [Options.ASSIGNED] : false,
+            [Options.ONLINE] : false,
+            [Options.OFFLINE] : false,
+            [Options.PRODUCT_ID] : { demandOption : false, describeFormatArgs : ['Devices'] },
+            [Options.PRODUCT_NAME] : { demandOption : false, describeFormatArgs : ['Devices'] },
+            [Options.DEVICE_GROUP_ID] : { demandOption : false, describeFormatArgs : ['Devices'] },
+            [Options.DEVICE_GROUP_NAME] : { demandOption : false, describeFormatArgs : ['Devices'] }
+        }))
+        .strict();
+};
+
+exports.handler = function (argv) {
+    if (!Options.checkCommandArgs(argv)) {
+        return;
+    }
+    new Device().list(new Options(argv));
+};
